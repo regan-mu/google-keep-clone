d9000ec9b819356ae346a856127952375cf1aa72
Regan254 <40056428+regan254@users.noreply.github.com>
2022-09-14 14:33:08 +0300

    Initial Commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import HighlightRoundedIcon from '@mui/icons-material/HighlightRounded';
+
+const Heading = () => {
+    return <header><h1><HighlightRoundedIcon /> Keeper</h1></header>;
+}
+export default Heading;
